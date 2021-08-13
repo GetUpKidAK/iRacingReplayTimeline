d75a8b01afc491ef2d4ff7eaea4d4a27e2f906a6
Ash <ash.kendall@gmail.com>
2021-08-13 02:38:49 +0100

    Updated version number

--- ReplayTimeline/ViewModel/ReplayDirectorVM.Properties.cs
+++ ReplayTimeline/ViewModel/ReplayDirectorVM.Properties.cs
@@ -12,7 +12,7 @@ namespace iRacingReplayDirector
 		private bool m_LiveSessionPopupVisible;
 
 		private const string m_ApplicationTitle = "iRacing Sequence Director";
-		private const string m_VersionNumber = "1.21";
+		private const string m_VersionNumber = "1.25";
 
 		/// <summary>
 		/// PROPERTIES
